f1087e1d88eecfebb2610efa3b2022a65b8e21a9
Safad T M <106598178+safadtm@users.noreply.github.com>
2024-02-01 11:11:09 +0530

    Create pg14.php

---
+++ pg14.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <?php
+        $colors = array("red","green","blue","yellow");
+
+        foreach($colors as $x){
+            echo "$x <br>";
+        }
+        ?>
+    </body>
+</html>
