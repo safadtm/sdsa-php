05efb3da7e458b6b3a2ecbefd689949aa693c6d3
Safad T M <106598178+safadtm@users.noreply.github.com>
2024-02-02 19:01:54 +0530

    Create pg8.php

---
+++ pg8.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <center>
+
+        <h3>
+            Program to print multiplication<br>
+            table
+        </h3>
+
+        <form method="POST">
+            Enter a number:
+            <input type="text" name="number">
+
+            <input type="Submit" value="Get Multiplication Table">
+        </form>
+    </center>
+    <?php
+    if ($_POST) {
+        $num = $_POST["number"];
+
+        echo nl2br("<p style='text-align: center;'> 
+   Multiplication Table of $num: </p>");
+
+        for ($i = 1; $i <= 10; $i++) {
+            echo ("<p style='text-align: center;'>$num"
+                . " X " . "$i" . " = "
+                . $num * $i . "</p>");
+        }
+    }
+    ?>
+</body>
+
+</html>
