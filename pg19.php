0b1c9828ce9a5c38b4d8c66f313abdb06f323ffd
Safad T M <106598178+safadtm@users.noreply.github.com>
2024-02-02 11:08:17 +0530

    Create pg19.php

---
+++ pg19.php
@@ -0,0 +1,18 @@
+<html>
+    <body>
+        <h2>Html Form Input Types</h2>
+        <form action="">
+            <label for="fname">First name:</label><input type="text" name="fname" id=""><br><br>
+            <label for="lname">Last name:</label><input type="text" name="lname" id=""><br><br>
+            <label for="password">Password:</label><input type="password" name="password" id=""><br><br>
+            <label for="address">Address:</label><textarea name="address" id="" cols="20" rows="5"></textarea><br><br>
+            <label for="number">Number:</label><input type="number" name="number" id=""><br><br>
+            <label for="email">Email:</label><input type="email" name="email" id=""><br><br>
+            <label for="date">Date:</label><input type="date" name="date" id=""><br><br>
+            <label for="file">File:</label><input type="file" name="file" id=""><br><br>
+            <label for="time">Time:</label><input type="time" name="file" id=""><br><br>
+            <br>
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
