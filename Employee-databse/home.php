07d3fe745b5ab79657ae3c6fece4c70dda10c40e
safadtm <safadtm2004@gmail.com>
2024-03-24 10:09:32 +0530

    home created

--- Employee-databse/home.php
+++ Employee-databse/home.php
@@ -1,16 +1,66 @@
+<!DOCTYPE html>
 <html>
-    <head><title>welcome home</title></head>
-<body><div class="form">
-    <p>welcome </p>
+
+<head>
+    <title>Welcome Home</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        .form {
+            width: 300px;
+            margin: 50px auto;
+            background: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+        }
+
+        p {
+            font-size: 18px;
+            color: #333;
+        }
+
+        .viewall {
+            display: block;
+            margin-top: 10px;
+            text-decoration: none;
+            color: #8A2BE2; 
+        }
+
+        .viewall:hover {
+            color: #9932CC;
+        }
+
+        .logout {
+            display: block;
+            margin-top: 10px;
+            text-decoration: none;
+            color: #f44336; 
+        }
+
+        .logout:hover {
+            color: #d32f2f;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="form">
+        <p>Welcome</p>
         <?php
         session_start();
- echo $_SESSION['name']; 
- ?>
+        echo $_SESSION['name'];
+        ?>
         <br>
-        <a href="viewall.php">view </a>
+        <a class="viewall" href="viewall.php">View All</a>
         <br>
-        <a href="logout.php">logout</a>
-        
+        <a class="logout" href="logout.php">Logout</a>
+    </div>
+</body>
 
-</div></body>
-    </html>
+</html>
