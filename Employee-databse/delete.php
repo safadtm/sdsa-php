014a39e57d747068acf411de834d75b49044c76f
safadtm <safadtm2004@gmail.com>
2024-03-24 14:21:16 +0530

    delete created

---
+++ Employee-databse/delete.php
@@ -0,0 +1,18 @@
+<html>
+<?php
+require('db.php');
+if(isset($_GET['id'])){
+  $delete_id=$_GET['id'];
+  $sql="delete from register where id='".$delete_id."'";
+  if(mysqli_query($conn,$sql)){
+    header("Location: viewall.php");
+    echo"Record was delete successfully.";
+    echo"<a href ='home.php'>Home</a>";
+
+  }else{
+    echo"ERROR:Could not able to execute $sql.".mysqli_error($conn);
+}
+}
+?>
+
+</html>
