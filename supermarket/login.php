f8be16d5de97bb7ac5646ec75fe26450b6c32c39
safadtm <safadtm2004@gmail.com>
2024-08-07 06:10:52 +0530

    sample

--- supermarket/login.php
+++ supermarket/login.php
@@ -35,7 +35,7 @@ if (isset($_POST['username'])) {
     $sql = "SELECT * FROM admins WHERE username='$username' AND password='$password'";
     
     $result = mysqli_query($conn,$sql) or die(mysqli_error($conn));
-
+           
     if(mysqli_num_rows($result)==1){
         $_SESSION['username']=$username;
          header("Location: home.php");
